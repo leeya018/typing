1f7028478a44ac8f9cc623f498579d0ccf7c3356
lee yahav <leeyahav018@gmail.com>
2022-12-26 09:46:26 +0200

    this is not checked

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,25 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
+import styled from "styled-components";
+import { Wrapper, Button, Rotate } from "./style1";
+
 export default function Index({}) {
   const router = useRouter();
   useEffect(() => {
-    router.push("/typing");
+    // router.push("/typing");
   }, []);
-  return <div>Index</div>;
+  return (
+    <Wrapper>
+      <div>Index</div>
+      <div>
+        <Button>Normal</Button>
+        <Button primary>Primary</Button>
+      </div>
+      <Rotate>&lt; 💅🏾 &gt;</Rotate>
+    </Wrapper>
+  );
+
 }
+
+// 1
